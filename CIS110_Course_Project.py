a971cdb53058b5065a18c0e5201c601fd1bbb88e
username <useremail>
2024-09-09 14:37:16 -0700

    Starting story line

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -1,1 +1,9 @@
+print("Welcome friends today I am going to tell you a story about a dog that takes a trip.")
+print("In the fields down below please answer all questions.")
+print("After you answer each question please hit the enter button.")
+print("Lets get started! ")
 
+
+breed = input(f"\n\n\nWhat breed is the dog: ")
+age = input(f"\nHow old is your new friend: ")
+sex = input(f"\nIs your friend a boy or girl: ")
