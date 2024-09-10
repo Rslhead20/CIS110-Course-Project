15f56ea4fd2d448953f87ff10793700ea44ce612
username <useremail>
2024-09-09 22:40:53 -0700

    Added more story lines

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -5,5 +5,13 @@ print("Lets get started! ")
 
 
 breed = input(f"\n\n\nWhat breed is the dog: ")
-age = input(f"\nHow old is your new friend: ")
+name = input(f"\nWhat is the name of your new friend: ")
+city = input(f"\nWhat city does your new friend live in: ")
+size = input(f"\nWhat is the size of the dog: ")
 sex = input(f"\nIs your friend a boy or girl: ")
+
+print(f"\n\n\n\nLets Begin. ")
+print(f"\nOne day {name} wakes up from taking a nap. ")
+print(f"\n{name} owners have never let them go outside as far as the backyard. ")
+print(f"\nYour name is {name} a {size} {breed}. ")
+
